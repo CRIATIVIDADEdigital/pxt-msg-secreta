fcc2e602e56e753b2162167eb4878ed3409a2465
Admin CRIATIVIDADE.digital <32722216+criatividade-digital@users.noreply.github.com>
2019-04-26 16:22:21 -0300

    Updates.

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-    basic.showString("Coloque seu nome aqui")
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Mensagem do botão A")
+    basic.showString("A")
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("Mensagem do botão B")
+    basic.showString("B")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
